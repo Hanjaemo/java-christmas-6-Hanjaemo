7f71efd416aaacb106347b77da51cf32738cfadb
Hanjaemo <hjm2530@gmail.com>
2023-11-12 02:42:34 +0900

    test(OrderTest): 할인 전 총주문 금액 게산 기능 단위 테스트

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -2,21 +2,28 @@ package christmas.domain;
 
 import java.util.Map;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
+    Map<Menu, Integer> orderMenus;
+    Order order;
+
+    @BeforeEach
+    void setUp() {
+        orderMenus = Map.of(
+                Menu.T_BONE_STEAK, 1,
+                Menu.CHOCOLATE_CAKE, 2,
+                Menu.COKE_ZERO, 2
+        );
+        order = new Order(orderMenus);
+    }
+
     @DisplayName("주문한 메뉴 중에서 카테고리가 메인인 메뉴들의 개수를 센다.")
     @Test
     void countMainMenus_Success() {
-        // given
-        Map<Menu, Integer> orderMenus = Map.of(
-                Menu.SEAFOOD_PASTA, 1,
-                Menu.CHAMPAGNE, 2
-        );
-        Order order = new Order(orderMenus);
-
         // when
         int mainMenusCount = order.countMainMenus();
 
@@ -27,14 +34,6 @@ class OrderTest {
     @DisplayName("주문한 메뉴 중에서 카테고리가 디저트인 메뉴들의 개수를 센다.")
     @Test
     void countDessertMenus_Success() {
-        // given
-        Map<Menu, Integer> orderMenus = Map.of(
-                Menu.TAPAS, 1,
-                Menu.CHOCOLATE_CAKE, 2,
-                Menu.COKE_ZERO, 2
-        );
-        Order order = new Order(orderMenus);
-
         // when
         int mainMenusCount = order.countDessertMenus();
 
@@ -42,4 +41,13 @@ class OrderTest {
         Assertions.assertThat(mainMenusCount).isEqualTo(2);
     }
 
+    @DisplayName("할인 전 총주문 금액을 계산한다.")
+    @Test
+    void calculateTotalOrderAmount_Success() {
+        // when
+        int totalOrderAmount = order.calculateTotalOrderAmount();
+
+        // then
+        Assertions.assertThat(totalOrderAmount).isEqualTo(91_000);
+    }
 }
