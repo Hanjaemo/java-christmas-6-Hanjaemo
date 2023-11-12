8b29711d863586a0341974f9d7281678fcc3d78c
Hanjaemo <hjm2530@gmail.com>
2023-11-13 00:28:53 +0900

    feat(OutputView): 할인 후 예상 결제 금액 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -74,4 +74,9 @@ public class OutputView {
         System.out.println("<총혜택 금액>");
         System.out.println(convertNumberToKoreanWonFormat(-totalBenefitAmount));
     }
+
+    public static void printExpectedPaymentAmountAfterDiscount(int expectedPaymentAmountAfterDiscount) {
+        System.out.println("<할인 후 예상 결제 금액>");
+        System.out.println(convertNumberToKoreanWonFormat(expectedPaymentAmountAfterDiscount));
+    }
 }
