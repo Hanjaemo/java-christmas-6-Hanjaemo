85b1dffa6a86a9faa8611bdb8c4a7be581d50bb7
Hanjaemo <hjm2530@gmail.com>
2023-11-12 23:37:22 +0900

    feat(OutputView): 이벤트 혜택 미리 보기 문구 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -8,4 +8,8 @@ public class OutputView {
     public static void printGreetingMessage() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+
+    public static void printPreviewOfEventBenefitsMessage(int visitDay) {
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", visitDay);
+    }
 }
