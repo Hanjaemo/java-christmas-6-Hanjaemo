aafd99687e653b074b8f68bff9a798d9265b2465
Hanjaemo <hjm2530@gmail.com>
2023-11-12 23:30:00 +0900

    feat(InputView): 주문할 메뉴와 개수 입력 기능 구현 및 예외 처리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,8 +1,13 @@
 package christmas.view;
 
+import java.util.regex.Pattern;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
+    private static final Pattern ORDER_MENUS_REGEX =
+            Pattern.compile("^([가-힣a-zA-Z0-9]+-\\d+)(,[가-힣a-zA-Z0-9]+-\\d+)*$");
 
     private InputView() {
     }
@@ -10,10 +15,35 @@ public class InputView {
     public static int readVisitDay() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
+        validateEmpty(input);
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
         }
+    }
+
+    private static void validateEmpty(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 값을 입력하지 않았습니다.");
+        }
+    }
+
+    public static String[] readOrderMenus() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = Console.readLine();
+        validateEmpty(input);
+        validateOrderMenusRegex(input);
+        return input.split(",");
+    }
+
+    private static void validateOrderMenusRegex(String input) {
+        if (invalidOrderRegex(input)) {
+            throw new IllegalArgumentException("[ERROR] 입력 형식이 일치하지 않습니다.");
+        }
+    }
+
+    private static boolean invalidOrderRegex(String input) {
+        return !ORDER_MENUS_REGEX.matcher(input).matches();
     }
 }
