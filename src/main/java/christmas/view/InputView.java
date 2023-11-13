0c7936d0d8d5b7184bbaf9d863dda23a50e65a5d
Hanjaemo <hjm2530@gmail.com>
2023-11-13 21:55:41 +0900

    refactor(InputView): 숫자 미입력 시 출력하는 에러 메시지 내용을 요구 사항에 따라 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -28,7 +28,7 @@ public class InputView {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 
