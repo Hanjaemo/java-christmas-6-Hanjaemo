8072ebcbac816b7e6dff8eacf541da477468a6e4
Hanjaemo <hjm2530@gmail.com>
2023-11-15 15:47:30 +0900

    refactor(InputView): 불필요한 검증 메서드 제거

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -22,14 +22,7 @@ public class InputView {
     public static int readVisitDay() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
-        validateEmpty(input);
         return toInt(input);
-    }
-
-    private static void validateEmpty(String input) {
-        if (input.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 값을 입력하지 않았습니다. 다시 입력해 주세요.");
-        }
     }
 
     private static int toInt(String input) {
@@ -43,7 +36,6 @@ public class InputView {
     public static OrderMenusDto readOrderMenus() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
-        validateEmpty(input);
         validateOrderMenusRegex(input);
         Map<String, Integer> orderMenus = Arrays.stream(input.split(ORDER_MENUS_DELIMITER))
                 .map(orderMenu -> orderMenu.split(MENU_AND_QUANTITY_DELIMITER))
