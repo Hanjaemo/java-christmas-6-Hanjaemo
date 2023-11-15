491ceb6cbc9eca0dae76daf8e795c37e72b02c71
Hanjaemo <hjm2530@gmail.com>
2023-11-15 15:48:25 +0900

    test(WeekDiscountEventManagerTest): WeekDiscountEventManager 할인 미적용 기능 단위 테스트

--- src/test/java/christmas/domain/event/WeekDiscountEventManagerTest.java
+++ src/test/java/christmas/domain/event/WeekDiscountEventManagerTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -52,6 +53,34 @@ class WeekDiscountEventManagerTest {
 
         // then
         Assertions.assertThat(discountAmount).isEqualTo(2023);
+    }
+
+    @DisplayName("방문 날짜가 평일이지만 디저트 메뉴가 없는 경우 이벤트를 적용하지 않으며, 할인 금액은 0원이다.")
+    @Test
+    void applyEvent_Fail_ByNotExistDessert() {
+        // given
+        OrderMenus orderMenus = new OrderMenus(List.of(new OrderMenu(Menu.T_BONE_STEAK, 1)));
+        CustomerInfo customerInfo = new CustomerInfo(createVisitDay(5), orderMenus);
+
+        // when
+        int discountAmount = eventManager.applyEvent(customerInfo, new BenefitDetails());
+
+        // then
+        Assertions.assertThat(discountAmount).isEqualTo(0);
+    }
+
+    @DisplayName("방문 날짜가 주말이지만 메인 메뉴가 없는 경우 이벤트를 적용하지 않으며, 할인 금액은 0원이다.")
+    @Test
+    void applyEvent_Fail_ByNotExistMain() {
+        // given
+        OrderMenus orderMenus = new OrderMenus(List.of(new OrderMenu(Menu.ICE_CREAM, 1)));
+        CustomerInfo customerInfo = new CustomerInfo(createVisitDay(9), orderMenus);
+
+        // when
+        int discountAmount = eventManager.applyEvent(customerInfo, new BenefitDetails());
+
+        // then
+        Assertions.assertThat(discountAmount).isEqualTo(0);
     }
 
     VisitDay createVisitDay(int visitDay) {
